2c15222c16620e258507ba7dd2f3eeb0cbfde95b
snaztoz <hafidhmn@gmail.com>
2021-04-19 21:41:04 +0700

    Menghubungkan kedua activity.

--- app/src/main/java/com/example/bengkel/BengkelListActivity.java
+++ app/src/main/java/com/example/bengkel/BengkelListActivity.java
@@ -2,7 +2,9 @@ package com.example.bengkel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BengkelListActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class BengkelListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bengkel_list);
+
+        Intent intent = getIntent();
+        String bengkelType = intent.getStringExtra(HomeActivity.BENGKEL_TYPE);
+
+        TextView bengkelListTitle = findViewById(R.id.bengkelListTitle);
+        bengkelListTitle.setText("Ready to fix your " + bengkelType);
     }
 }
